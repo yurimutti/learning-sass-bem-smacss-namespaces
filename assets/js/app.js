5c9c75c58029f6037d2f87e902deb2741883e01e
yurimutti <muttiyuri@gmail.com>
2020-12-02 01:26:02 -0300

    mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,8 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
+// const hamburger = document.getElementById('hamburger');
+// const navUL = document.getElementById('navUL');
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
 
-function show(){
- mainMenu.style.display = 'flex';
- mainMenu.style.top = '0';
- }
- function close(){
-     mainMenu.style.top = '-100%';
- }
+
+// hamburger.addEventListener('click', () => {
+//     navUL.classList.toggle('show');
+// });
